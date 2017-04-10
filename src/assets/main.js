752d177702ea2fcdbeb1e207c24ec8b7274d034f
nicolashaccenture <nicolas.huzella@accenture.com>
2017-04-10 19:59:18 +0100

    first attempt

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,67 @@
 $(function() {
 
-  // your code will go here
+$.ajax({
+    url: 'https://www.codeschool.com/users/3645491.json',
+    dataType: 'jsonp',
+    success: function(response) {
+    	console.log(response, 'response')
+    	addCourses(response.courses.completed)
+    	addBadges(response.badges)
+      // handle response
+    }
+  });
+
+function addCourses(courses) {
+
+	var $badges = $('#badges');
+
+	courses.forEach(function(course) {
+
+		var $course = $('<div />', {
+			'class': 'course'
+		}).appendTo($badges);
+
+		$('<h3 />', {
+			text: course.title
+		}).appendTo($course);
+
+		$('<img />', {
+			src: course.badge
+		}).appendTo($course)
+
+		$('<a />', {
+			'class': 'btn btn-primary',
+			target: '_blank',
+			href: course.url,
+			text: 'See Course'
+		}).appendTo($course)
+
+
+	})
+
+
+	}
+
+function addBadges(courses) {
+
+var $badges = $('#badges');
+
+	courses.forEach(function(course) {
+		var $badge = $('<div />', {
+			'class': 'course'
+		}).appendTo($badges);
+
+	//	$('<h3 />', {
+	//		text: course.title
+	//	}).appendTo($course);
+
+
+	})
+
+	}
+
+
 
 });
+
+
